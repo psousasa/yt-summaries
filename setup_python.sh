7013ab5ba38947e486481bedd7ecc0ede0da3c9d
psousasa <pedro.sa@loka.com>
2024-08-28 13:50:23 +0100

    added streamlit and python container init

---
+++ setup_python.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get the latest .whl file
+latest_whl=$(ls dist/yt_summaries-*-py3-none-any.whl | sort -V | tail -n 1)
+
+# Extract the version from the .whl file name (assumes version follows the pattern yt_summaries-X.Y.Z)
+whl_version=$(echo $latest_whl | sed -n 's/.*yt_summaries-\([0-9.]*\)-py3-none-any.whl/\1/p')
+
+# Check if the package is already installed and get the installed version
+installed_version=$(pip show yt_summaries | grep Version | awk '{print $2}')
+
+# Compare the installed version with the .whl version
+if [ "$installed_version" == "$whl_version" ]; then
+    echo "🟢 The latest version ($installed_version) is already installed. Skipping installation."
+else
+    echo "🔴 Installing latest dist... ($whl_version)"
+    pip install "$latest_whl"
+    echo "🟢 Done!"
+fi
+
+echo "Running Streamlit App"
+streamlit run app/yt_info/streamlit_app.py --server.port=8501 --server.address=0.0.0.0
